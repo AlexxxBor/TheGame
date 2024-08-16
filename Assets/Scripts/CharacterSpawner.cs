643f4c2237f272b086af30c09331d5dda1947214
Alexey <abzh.1805@gmail.com>
2024-08-16 16:47:24 +0300

    CharacterSpawner class completed
    Сделал класс для размножения персонажей в столбик с отступами.

---
+++ Assets/Scripts/CharacterSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSpawner : MonoBehaviour
+{
+    public Character Character;
+    public float OffsetBelow = 0.5f;
+    public float OffsetBetween = 0.1f;
+
+    private void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Character instantiate = Instantiate(Character, new Vector3(2, i + i * OffsetBetween + OffsetBelow, -2), Quaternion.identity);
+            Debug.LogWarning($"Character with name {instantiate.name} created");
+        }
+    }
+}
