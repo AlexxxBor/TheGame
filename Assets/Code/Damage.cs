6125dd52989510da2d9f7d4522cce35ac0d42937
Alexey <abzh.1805@gmail.com>
2024-08-16 11:59:58 +0300

    Damage and ValuesOutput
    Создал классы Damage и ValuesOutput. Первый срабатывает при запуске сцены, второй - через OnValidate().

---
+++ Assets/Code/Damage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Assets.Code
+{
+    internal class Damage : MonoBehaviour
+    {
+        public int BaseDamage = 10;
+        public float Multiplier = 1.0f;
+
+        private void Start()
+        {
+            Debug.LogWarning($"Нанесённый урон: {CalculateDamage(BaseDamage, Multiplier)}");
+        }
+
+        private float CalculateDamage(int damage, float multiplier) => damage * multiplier;
+    }
+
+}
---
+++ Assets/Code/ValuesOutput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Code
+{
+    internal class ValuesOutput : MonoBehaviour
+    {
+        public int Integer;
+        public float Float;
+        public bool Boolean;
+
+        private void OnValidate()
+        {
+            Debug.Log($"Целочисленное значение теперь: {Integer}");
+            Debug.Log($"Новое число с плавающей точкой: {Float}");
+            Debug.Log($"Булево значение равно: {Boolean}");
+        }
+    }
+}
