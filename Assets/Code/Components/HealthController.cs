d85bb34d6371097a0d4835c71c4cc80866abc375
Alexey <abzh.1805@gmail.com>
2024-09-07 20:01:12 +0300

    Сделаны компоненты оружия
    WeaponController, Weapon и Bullet

---
+++ Assets/Code/Components/HealthController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Components
+{
+    internal class HealthController : MonoBehaviour
+    {
+        [SerializeField] private int _health;
+
+        private bool _isAlive = true;
+
+        public bool CanTakeDamage(int damage)
+        {
+            if (_isAlive == false)
+            {
+                return false;
+            }
+
+            _health -= damage;
+
+            if (_health <= 0)
+            {
+                _isAlive = false;
+                return false;
+            }
+
+            return true;
+        }
+        
+    }
+}
---
+++ Assets/Code/Weapon/Bullet.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Weapon
+{
+    [RequireComponent(typeof(Rigidbody))]
+
+    internal class Bullet : MonoBehaviour
+    {
+        private Rigidbody _rigidBody;
+        private bool _isActive;
+
+        private void Awake()
+        {
+            _rigidBody = GetComponent<Rigidbody>();
+        }
+
+        private void OnBecameInvisible()
+        {
+            if(_isActive == false)
+            {
+                return;
+            }
+
+            Destroy(gameObject);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            Destroy(gameObject);
+        }
+
+        internal void Run(Vector3 path, Vector3 startPosition)
+        {
+            transform.position = startPosition;
+
+            gameObject.SetActive(true);
+            _rigidBody.WakeUp();
+            _rigidBody.AddForce(path, ForceMode.Impulse);
+
+            _isActive = true;
+        }
+        public void Sleep()
+        {
+            _rigidBody.Sleep();
+            gameObject.SetActive(false);
+            _isActive = false;
+        }
+    }
+}
