cabfbae8e789290ddc2ed9fc7fad0b02afcb4b6b
Alexey <abzh.1805@gmail.com>
2024-09-09 10:14:51 +0300

    Refactoring
    Сделал единый компонент поворота для обоих типов передвижения. Создал компонент здоровья.

--- Assets/Code/Components/Rotation/RotationComponent.cs
+++ Assets/Code/Components/Rotation/RotationComponent.cs
@@ -1,7 +1,28 @@
 using UnityEngine;
 
-public abstract class RotationComponent : MonoBehaviour
+public class RotationComponent : MonoBehaviour
 {
     public float RotationSpeed = 5f;
     public Vector3 RotationDirection;
+
+    private float _deltaAngle;
+    private Quaternion _currentRotation;
+    private Quaternion _targetRotation;
+    private Quaternion _newRotation;
+
+    void Update()
+    {
+        if (RotationDirection == Vector3.zero)
+        {
+            return;
+        }
+
+        _deltaAngle = RotationSpeed * Time.deltaTime;
+
+        _currentRotation = transform.rotation;
+        _targetRotation = Quaternion.LookRotation(RotationDirection);
+        _newRotation = Quaternion.RotateTowards(_currentRotation, _targetRotation, _deltaAngle);
+
+        transform.rotation = _newRotation;
+    }
 }
