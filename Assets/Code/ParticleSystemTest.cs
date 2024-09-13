a529c930f0598fce9dc8c0fb8d5e1c2727564c7e
Alexey <abzh.1805@gmail.com>
2024-09-13 17:22:40 +0300

    Explosion test script created.

---
+++ Assets/Code/ParticleSystemTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Assets.Code
+{
+    internal class ParticleSystemTest : MonoBehaviour
+    {
+        [SerializeField] private GameObject _explosionEffect;
+        [SerializeField] private bool _activate = false;
+
+        private GameObject _effectCopy;
+        private ParticleSystem _particles;
+
+        private void Awake()
+        {
+            _effectCopy = Instantiate(_explosionEffect, transform);
+            _effectCopy.transform.SetParent(null);
+
+            _particles = _effectCopy.GetComponent<ParticleSystem>();
+        }
+
+        private void OnValidate()
+        {
+            if (_activate)
+            {
+                _particles.Play();
+            }
+        }
+    }
+}
